d0da38b38fbc6df993aaef42168a4950f7002c80
Paridhipatidar03 <patidarparidhi35@gmail.com>
2025-07-17 12:50:46 +0530

    Security Filter Chain updated for corsconfig

--- Backend/src/main/java/student_info/configuration/WebConfig.java
+++ Backend/src/main/java/student_info/configuration/WebConfig.java
@@ -1,17 +1,17 @@
-package student_info.configuration;
-
-//import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") 
-                .allowedOrigins("*") 
-                .allowedMethods("*") 
-                .allowedHeaders("*"); 
-    }
-}
+//package student_info.configuration;
+//
+////import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") 
+//                .allowedOrigins("*") 
+//                .allowedMethods("*") 
+//                .allowedHeaders("*"); 
+//    }
+//}
